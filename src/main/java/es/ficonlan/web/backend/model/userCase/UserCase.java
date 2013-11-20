f496022a958a6468555fbee99d720b7eabe264bc
MiguelAngelCastilloBellagona <surah.harus@gmail.com>
2013-11-20 22:29:14 +0100

    UserCase

---
+++ src/main/java/es/ficonlan/web/backend/model/userCase/UserCase.java
@@ -0,0 +1,48 @@
+package es.ficonlan.web.backend.model.userCase;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ * @author Miguel Ángel Castillo Bellagona
+ * @version 1.0
+ */
+@Entity
+public class UserCase {
+	
+	private long userCaseId;
+	private String userCaseName;
+	
+	public UserCase() {};
+	
+	public UserCase(String name)
+	{
+		super();
+		this.userCaseName = name;
+	}
+	
+	@Column(name = "userCaseId")
+	@SequenceGenerator(name = "userCaseIdGenerator", sequenceName = "userCaseSeq")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "userCaseIdGenerator")
+	public long getUserCaseId() {
+		return this.userCaseId;
+	}
+	
+	public void setUserCaseId(long newId)
+	{
+		this.userCaseId = newId;
+	}
+	
+	@Column(name = "userCaseName")
+	public String getUserCaseName()
+	{
+		return this.userCaseName;
+	}
+
+}
