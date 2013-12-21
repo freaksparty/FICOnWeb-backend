85bb3c9bd2b8e3a781ef3356f84df7980fc57a83
Miguel Angel Castillo Bellagona <surah.harus@gmail.com>
2013-12-21 19:36:44 +0100

    Puto git

---
+++ src/main/java/es/ficonlan/web/backend/model/activity/ActivityDao.java
@@ -0,0 +1,21 @@
+package es.ficonlan.web.backend.model.activity;
+
+import java.util.List;
+
+import es.ficonlan.web.backend.model.util.dao.GenericDao;
+
+/**
+ * @author Miguel Ángel Castillo Bellagona
+ * @version 1.0
+ */
+public interface ActivityDao extends GenericDao<Activity, Integer> {
+	
+	
+	/**
+	 * @param eventId Id del evento del que queremos obtener la lista de Actividades (si es 0 se devolverán las de todos los eventos)
+	 * @param type Tipo de actividades que queremos obtener (si es 0 se devolverán todos los tipos)
+	 * @return List<Activity>
+	 */
+	public List<Activity> findActivitiesByEventByType(long eventId, int type);
+
+}
