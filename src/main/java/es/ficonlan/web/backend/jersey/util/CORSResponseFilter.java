9846a0a2f51e8c363a5652a48bd3bb5c89848277
MiguelAngelCastilloBellagona <surah.harus@gmail.com>
2014-11-03 19:43:52 +0100

    asd

--- src/main/java/es/ficonlan/web/backend/jersey/util/CORSResponseFilter.java
+++ src/main/java/es/ficonlan/web/backend/jersey/util/CORSResponseFilter.java
@@ -20,8 +20,8 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         headers.add("Access-Control-Allow-Origin", "*");
         //headers.add("Access-Control-Allow-Origin", "http://ficonlan.es");        
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");    
-        //headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, sessionId");
-        headers.add("Access-Control-Allow-Headers", "sessionId");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, sessionId");
+        //headers.add("Access-Control-Allow-Headers", "sessionId");
     }
  
 }
