fec0b79bce25842a676beb6cfefed689da5a2f23
MiguelAngelCastilloBellagona <surah.harus@gmail.com>
2014-11-12 17:26:12 +0100

    s

--- src/main/java/es/ficonlan/web/backend/model/newsitem/NewsDaoHibernate.java
+++ src/main/java/es/ficonlan/web/backend/model/newsitem/NewsDaoHibernate.java
@@ -94,14 +94,16 @@ public class NewsDaoHibernate extends GenericDaoHibernate<NewsItem, Integer> imp
 		        ).setParameter("eventId", eventId).setParameter("now", now).setFirstResult(startIndex).setMaxResults(cont).list();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public int getAllPublishedNewsItemFromEventTam(int eventId) {
 		Calendar now = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		return (int) getSession().createQuery(
-	        	"SELECT cout(n) " +
+		List<NewsItem> l = getSession().createQuery(
+	        	"SELECT n " +
 		        "FROM NewsItem n WHERE n.event.eventId = :eventId" 
 	        	+ " AND n.publishDate <= :now"
 	        	+ " ORDER BY n.publishDate DESC"
-		        ).setParameter("eventId", eventId).setParameter("now", now).uniqueResult();
+		        ).setParameter("eventId", eventId).setParameter("now", now).list();
+		return l.size();
 	}
 	
 }
